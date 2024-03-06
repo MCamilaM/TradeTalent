767a3d08ed6a8dfc98ea23df62b209d7e07768cc
Maria Morales <camila.moralesg21@gmail.com>
2024-03-05 22:35:47 -0500

    add: se instala dependencias para android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.tradetalent.app',
   appName: 'tradetalent',
   webDir: 'www',
   server: {
